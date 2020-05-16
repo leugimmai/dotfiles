a0f62475eecde17d4ed494c0427b480571ee4d6c
Miguel Quintana <mqguitar@me.com>
2020-05-15 20:58:59 -0500

    updated my vim setup and config

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+set -e
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+cp ./.vimrc ~/.vimrc
+cp -a ./.vim_runtime/. ~/.vim_runtime/
